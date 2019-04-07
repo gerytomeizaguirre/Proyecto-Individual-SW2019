9ae9152bee6d41842790d37737b4b1ff60d87a4c
Gery <gery.tomeizaguirre@gmail.com>
2019-04-07 00:19:22 -0600

    fourth commit

--- app/src/main/java/cronometro/android/aplicacion/MainActivity.java
+++ app/src/main/java/cronometro/android/aplicacion/MainActivity.java
@@ -2,12 +2,18 @@ package cronometro.android.aplicacion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_start, btn_stop, btn_reset;
+    Chronometer cronometro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
