d6876d581273172fe95b2bb7664d89973a6058d1
Gery <gery.tomeizaguirre@gmail.com>
2019-04-07 01:11:37 -0600

    fifth commit

--- app/src/main/java/cronometro/android/aplicacion/MainActivity.java
+++ app/src/main/java/cronometro/android/aplicacion/MainActivity.java
@@ -1,19 +1,70 @@
 package cronometro.android.aplicacion;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_start, btn_stop, btn_reset;
-    Chronometer cronometro;
+    Chronometer chronometro;
+    Boolean correr=false;
+    long detenerse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        btn_start=findViewById(R.id.btn_start);
+        btn_stop=findViewById(R.id.btn_stop);
+        btn_reset=findViewById(R.id.btn_reset);
+        chronometro=findViewById(R.id.chronometro);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startChronometro();
+            }
+        });
+
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopChronometro();
+            }
+        });
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetChronometro();
+            }
+        });
     }
+
+    private void stopChronometro() {
+        if(correr){
+            chronometro.stop();
+            detenerse = SystemClock.elapsedRealtime() - chronometro.getBase();
+            correr=false;
+        }
+    }
+
+    private void startChronometro() {
+        if(!correr){
+            chronometro.setBase(SystemClock.elapsedRealtime() - detenerse);
+            chronometro.start();
+            correr=true;
+        }
+    }
+
+    private void resetChronometro() {
+        chronometro.setBase(SystemClock.elapsedRealtime());
+        detenerse = 0;
+
+    }
+
 }
