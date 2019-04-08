dee70b08e35decae7e2778accb8d8cbff5baeefb
Gery <gery.tomeizaguirre@gmail.com>
2019-04-07 23:20:33 -0600

    seventh commit

---
+++ app/src/main/java/cronometro/android/aplicacion/Temporizador.java
@@ -0,0 +1,13 @@
+package cronometro.android.aplicacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Temporizador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temporizador);
+    }
+}
